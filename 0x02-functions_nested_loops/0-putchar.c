d8aeec646071f9a00512c4811c1d44f2c04f34f6
CharleS-ALX-GITHUB-ACC <koenigalfredcharles@gmail.com>
2022-03-16 14:32:00 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-public int main(void)
+/**
+* main - the main function of the program
+* @void: returns no value for this function
+* Description: An empty function that prints _putchar
+* Return: 0 there's no value for this function
+*/
+int main(void)
 {
 printf("_putchar\n");
 return (0);
