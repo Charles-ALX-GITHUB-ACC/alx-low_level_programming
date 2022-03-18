d5024e7bc2bb0a315a34bb7f3bcfe2ffc231a14b
Charles-ALX-GITHUB-ACC <99976542+Charles-ALX-GITHUB-ACC@users.noreply.github.com>
2022-03-18 05:46:38 -0700

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
-int main(void)
-{
-printf("\n");
-return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+/** _putchar - a fuction of type char */
+int _putchar(char r);
+/** print_alphabet - a function to print the letters of the alphabet */
+void print_alphabet(void);
