457c2d0f1e7d656c8feb1964bbb79306205bfe03
Charles-ALX-GITHUB-ACC <99976542+Charles-ALX-GITHUB-ACC@users.noreply.github.com>
2022-03-22 11:48:12 -0700

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,18 @@
-#ifendif MAIN_H
+#ifndif MAIN_H
 #define MAIN_H
+/** 
+* reset_to_98 - reset the value of 'n' after a function call
+*
+* Return: nothing
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
 }
+/**
+* _putchar - empty function
+*
+* Return: nothing
+*/
+void _putchar(void);
 endif
