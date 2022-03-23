ab0039509332cb634622e77048fb4870ef5f8a68
Charles-ALX-GITHUB-ACC <koenigalfredcharles@gmail.com>
2022-03-23 14:13:18 +0200

    Update 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h" 
+/**
+* swap_int - swaps the values 
+* @a: swaps the value of a to b 
+* @b: swaps the value of b to a
+*
+* Return: Always return nothing
+*
+*/
+void swap_int(int *a, int *b)
+{
+int c; 
+c = 0; 
+c = *a; 
+*a = *b; 
+*b = c; 
+}
