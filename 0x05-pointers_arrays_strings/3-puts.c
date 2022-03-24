7a887fde89959fc1feb02aff3dc152d59e8e725d
Charles-ALX-GITHUB-ACC <koenigalfredcharles@gmail.com>
2022-03-24 12:22:29 +0200

    update 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** 
+* _puts - a function to print a str to the standard output
+* @str: a string variable to a string value
+*
+* Return: string value 
+*/ 
+void _puts(char *str)
+{
+puts(str);
+}
+
