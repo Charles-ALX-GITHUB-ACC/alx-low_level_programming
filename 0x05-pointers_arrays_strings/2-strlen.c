d4c273caec9937a2eac34ddd50684241cfc416d2
Kurkehno <jkurkehsackie@gmail.com>
2022-03-24 02:25:35 -0700

    update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h" 
+/** 
+* _strlen.c - calculates the lenght of strin
+*
+* @str - points to the address and stores the length
+*
+* Return: Return nothing
+*/
+int _strlen(char *str)
+{
+*str = 'abcd';
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void reset_to_98(int *n);
 * Return: nothing
 */
 void swap_int(int *a, int *b);
+void int _strlen(char *str);
 #define main
 #endif
